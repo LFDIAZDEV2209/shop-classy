f87e69202331c10918080d565b5722769de06e61
LFDIAZDEV2209 <201288669+LFDIAZDEV2209@users.noreply.github.com>
2025-09-01 11:59:27 -0500

    Add 'Choose Us' section to dashboard with features highlighting natural ingredients, dermatological testing, free shipping, and premium quality. Update styles for performance optimizations and transitions.

---
+++ src/app/dashboard/components/chooseus-section/chooseus-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: 'chooseus-section',
+  templateUrl: './chooseus-section.component.html',
+  imports: [CommonModule],
+  standalone: true
+})
+export class ChooseusSectionComponent {
+    
+}
--- src/app/dashboard/pages/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { DashboardBannerComponent } from "../components/dashboard-banner/dashboard-banner.component";
 import { ProductsSectionsComponent } from "../components/products-sections/products-sections.component";
+import { ChooseusSectionComponent } from "../components/chooseus-section/chooseus-section.component";
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  imports: [DashboardBannerComponent, ProductsSectionsComponent],
+  imports: [DashboardBannerComponent, ProductsSectionsComponent, ChooseusSectionComponent],
 })
 
 export class DashboardComponent {
